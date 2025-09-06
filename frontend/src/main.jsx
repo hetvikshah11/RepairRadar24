d74993f0325363e74e2d3acba80be70ca2b24bdd
Hetvik Shah <hetvikshah11@gmail.com>
2025-09-06 19:07:57 +0530

    Page for configuring jobcard layout on dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import SignIn from './SignIn/SignIn.jsx';
 import Dashboard from './Dashboard/Dashboard.jsx';
 import JobCardConfig from './JobCardConfig/JobCardConfig.jsx';
 import CreateJobCard from './CreateJobCard/CreateJobCard.jsx';
+import JobCardLayoutConfig from './JobCardLayoutConfig/JobCardLayoutConfig.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/config" element={<JobCardConfig />} />
         <Route path="/create-job" element={<CreateJobCard />} />
+        <Route path="/layout-config" element={<JobCardLayoutConfig />} />
       </Routes>
     </Router>
   </React.StrictMode>,
